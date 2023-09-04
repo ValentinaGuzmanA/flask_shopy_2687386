10d062889eea8b4d749865c4859511302fa2b2c8
daniielaCR <naniscr0416@gmail.com>
2023-09-04 11:18:08 -0500

    producto

--- app/productos/forms.py
+++ app/productos/forms.py
@@ -1,6 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField,IntegerField,StringField, IntegerField
+from wtforms.validators import InputRequired, NumberRange
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
 class NewProductForm(FlaskForm):
-    nombre = StringField("Ingrese nombre del producto")
-    precio =IntegerField("Ingrese el precio del producto")
+    nombre = StringField(
+                        "Ingrese nombre del producto" ,
+                        validators=[
+                            InputRequired(message='nombre requerido')
+                                    ]
+                        
+                        
+                        )
+    precio =IntegerField("Ingrese el precio del producto",
+                         validators=[
+                             InputRequired(
+                                 message="Precio requerido"
+                                 ),
+                             NumberRange(
+                                 message="Precio fuera de rango",
+                                 min =10000,
+                                 max =100000
+                                        )
+                                     ]
+                        )
+    imagen = FileField (label="Imagen del pproducto ",
+                        validators=[FileRequired(message="Se requiere la imagen"),
+                                    FileAllowed(
+                                        ["jpg","png"],
+                                        message="solo se aceptan imagenes"
+
+                                    )
+                                    ]
+                       )
     submit =SubmitField("Guardar")
--- app/productos/routes.py
+++ app/productos/routes.py
@@ -1,10 +1,27 @@
 
 from flask import render_template
 from app.productos import productos
+import app
+import os
 from .forms import NewProductForm
 
-@productos.route('/crear')
+@productos.route('/create',
+                 methods=['GET','POST'])
 def crear():
+    p= app.models.Producto()
     form =NewProductForm()
+    if form.validate_on_submit():
+        form.populate_obj(p)
+        p.imagen=form.imagen.data.filename
+        app.db.session.add(p)
+        app.db.session.commit()
+        #subir imagen 
+        #campo de imagen (filestorage):
+
+        archivo= form.imagen.data
+        archivo.save(os.path.abspath(os.getcwd() +"/app/productos/imagenes/"+p.imagen))
+
+
+        return "producto registrado "
     return render_template('new.html',
                            form=form )
